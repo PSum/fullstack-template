48cc0c92aeedf41127d564a867e0952cdd468c68
psum <sum.philipp@gmail.com>
2025-04-13 16:13:25 +0200

    update
    changed docker-compose file before upgrading it with the GPT optimized
    version

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("../../generated/prisma_client");
 const prisma = new PrismaClient()
 
 
@@ -13,6 +13,36 @@ router.get('/', async (req, res) => {
     console.log(err);
     res.status(500).send('There has been an error' + err)
   }
+})
+
+router.get('/userAmount', async (req, res) => {
+    try {
+        const userCount = await prisma.user.count()
+  res.json(
+    userCount == 0
+      ? "No users have been added yet."
+      : "Some users have been added to the database."
+  );
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('There has been an error' + err)
+  }
+})
+
+router.post('/addUser', async (req, res) => {
+    try {
+        const {username} = req.body;
+        const data = await prisma.user.create({
+            data: {
+            username: username,
+            }
+        })
+        res.status(201).send(data);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('There has been an error')
+    }
 })
 
 module.exports = router;
